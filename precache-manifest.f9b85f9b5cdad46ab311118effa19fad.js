dc79168c0a5ee37127be1484b07c7bb84bc3a1ec
sheilaTing <835113231@qq.com>
2020-08-04 12:02:12 +0800

    deploy

---
+++ precache-manifest.f9b85f9b5cdad46ab311118effa19fad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdb38297df7ebd06e0b",
+    "url": "/deploy-record-account/css/app.bf267a12.css"
+  },
+  {
+    "revision": "b2089767d4f2ec9c2393c32aca1c2cd3",
+    "url": "/deploy-record-account/img/qrcode.png"
+  },
+  {
+    "revision": "433afd10e4af95c10243a09b10eb18d0",
+    "url": "/deploy-record-account/index.html"
+  },
+  {
+    "revision": "efdb38297df7ebd06e0b",
+    "url": "/deploy-record-account/js/app.a315f92c.js"
+  },
+  {
+    "revision": "02f0b2edba8b3150fa33",
+    "url": "/deploy-record-account/js/chunk-vendors.42589ae8.js"
+  },
+  {
+    "revision": "852fdb4ffb1dbaaf5c12eebd3902c8d1",
+    "url": "/deploy-record-account/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deploy-record-account/robots.txt"
+  }
+]);
